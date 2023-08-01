993ecbd0e4261120152b6adea2e3f57a46e3389c
EdSimonn <dami25edusi@gmail.com>
2023-08-01 10:41:06 -0700

    style: :construction_worker: truly

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -19,13 +19,10 @@ const Accordion: React.FC = () => {
   };
 
   return (
-    <div className=" dark:bg-neutral-800">
+    <div className="">
       {footerItems.map((item, index) => (
-        <div
-          key={index}
-          className="border-b border-neutral-200 dark:bg-neutral-800"
-        >
-          <div className="border-bottom">
+        <div key={index}>
+          <div>
             <div
               className={`group relative flex w-full items-center font-semibold bg-white px-5 py-4 text-left text-xs dark:bg-neutral-800 dark:text-white ${
                 item.isActive ? "bg-white" : ""
@@ -43,8 +40,8 @@ const Accordion: React.FC = () => {
                 />
               </span>
             </div>
+            <hr className="my-1 border-gray-200 sm:mx-auto dark:border-gray-700" />
           </div>
-          <div className="w-full border-dashed" />
           <div className={`${item.isActive ? "visible" : "hidden"} border-0 `}>
             <ul className="mt-4 px-5">
               {item.links.map((link, index) => (
@@ -61,23 +58,6 @@ const Accordion: React.FC = () => {
           </div>
         </div>
       ))}
-
-      <div className="flex flex-col px-5 py-5">
-        <h4 className="text-white font-semibold text-sm">NEWSLETTER SIGN UP</h4>
-        <p className="mt-4 text-sm text-gray-200 ">
-          Sign up for exclusive updates, new arrivals and insider only discounts
-        </p>
-        <div className="flex gap-4 mt-4">
-          <input
-            placeholder="enter your email address"
-            type="text"
-            className="w-[60%] py-2 p-2.5 text-white outline-none border-[1px] border-gray-200 bg-neutral-800"
-          />
-          <button className="py-2 px-6 bg-white text-sm font-semibold">
-            SUBMIT
-          </button>
-        </div>
-      </div>
     </div>
   );
 };
